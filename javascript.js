c96b2b35279ce369549d31e495a90170660c359c
Iainw03 <143198825+Iainw03users.noreply.github.com>
2024-07-28 14:26:58 +0100

    added button logic to removebook from list

--- javascript.js
+++ javascript.js
@@ -3,8 +3,11 @@ const myLibrary = [];
 function Book(title,author) {
   this.title = title;
   this.author = author;
+  this.bookId = `book${++Book.id}`;
 
 }
+
+Book.bookID = 0;
 
 function addBookToLibrary(title,author) {
   let bookDetails = new Book(title,author);
@@ -12,18 +15,39 @@ function addBookToLibrary(title,author) {
   displayBooks();
 }
 
+
+
 function displayBooks(){
     let i = myLibrary.length;
         let title = myLibrary[i];
      const Book = document.querySelector(".book")
          const BookBox = document.createElement("div");
+         BookBox.classList.add(`${myLibrary.bookId}`);
     BookBox.classList.add("bookCover");
     Book.appendChild(BookBox);
 
     const BookTitle = document.createElement("div");
     BookBox.appendChild(BookTitle);
 
+    const remBtn = document.createElement('button');
+    BookBox.appendChild(remBtn);
+    remBtn.textContent = 'Remove';
+    remBtn.onclick = remBook;
+
 }
+
+function remBook() {
+  const bookId = this.parentElement.classList[1];
+
+  const findBook = myLibrary.findIndex(
+    (element) => element.bookId === bookId
+  );
+  const delBook = myLibrary.splice(findBook, 1);
+  this.parentElement.remove();
+}
+  
+
+
 
 const dialog = document.querySelector("dialog");
 const bookButton = document.querySelector(".openbutton");
@@ -43,6 +67,8 @@ createBook.addEventListener("submit", ()=>{
   event.preventDefault();
 const author = createBook.author.value
 const title = createBook.title.value;
+
+
 console.log(author);
 
 addBookToLibrary(title, author);
